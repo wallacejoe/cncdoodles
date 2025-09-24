9b39fa848621f3a7d65d83632a933a9b30e8a3c9
wallacejoe <JAWcoder@gmail.com>
2025-09-24 13:18:12 -0700

    Improved carousel style. Added a name element.

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,20 +1,27 @@
 // Items to populate the carousel with
+// This list will be updated with new litter information
 const items = [
-    'Website Pictures/20220602_205650.jpg',
-    'Website Pictures/20221102_122424.jpg',
-    'Website Pictures/20220602_205650.jpg',
-    'Website Pictures/Nali Litter 2/Opal (3 Weeks).jpg',
-    'Website Pictures/20221102_122424.jpg',
+    { name: 'Xena', link: 'Website Pictures/20220602_205650.jpg'},
+    { name: 'Group', link: 'Website Pictures/20221102_122424.jpg'},
+    { name: 'Xena', link: 'Website Pictures/20220602_205650.jpg'},
+    { name: 'Opal', link: 'Website Pictures/Nali Litter 2/Opal (3 Weeks).jpg'},
+    { name: 'Group', link: 'Website Pictures/20221102_122424.jpg'},
 ];
 
 // Create the carousel items and html
 const carousel = document.querySelector('.carousel');
 
 items.forEach(item => {
-    const slide = document.createElement('div');
+    const slide = document.createElement('img');
     slide.classList.add('carousel-slide');
-    slide.style.backgroundImage = `url('${item}')`;
+    slide.src = item.link;
+    slide.alt = item.link;
     carousel.appendChild(slide);
+
+    const name = document.createElement('p');
+    name.classList.add('carousel-name');
+    name.textContent = `Name: ${item.name}`;
+    carousel.appendChild(name);
 })
 
 // Carousel navigation, created with help from copilot
@@ -22,9 +29,15 @@ let currentIndex = 0;
 const totalItems = items.length;
 
 const updateCarousel = () => {
+    // Update the displayed image
     const slides = document.querySelectorAll('.carousel-slide');
     slides.forEach((slide, index) => {
         slide.style.display = index === currentIndex? 'block' : 'none';
+    });
+    // Update the displayed name
+    const names = document.querySelectorAll('.carousel-name');
+    names.forEach((name, index) => {
+        name.style.display = index === currentIndex? 'block' : 'none';
     });
 };
 
