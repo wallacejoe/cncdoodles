a5d36ec6d6af7d2e6a3d10aad5f724ec6a99d9fc
wallacejoe <JAWcoder@gmail.com>
2025-10-02 15:06:39 -0700

    Tentatively finished the carousel, including the mailto link. Updated copyright on the home page.

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,11 +1,11 @@
 // Items to populate the carousel with
 // This list will be updated with new litter information
 const items = [
-    { name: 'Xena', link: 'Website Pictures/20220602_205650.jpg'},
-    { name: 'Group', link: 'Website Pictures/20221102_122424.jpg'},
-    { name: 'Xena', link: 'Website Pictures/20220602_205650.jpg'},
-    { name: 'Opal', link: 'Website Pictures/Nali Litter 2/Opal (3 Weeks).jpg'},
-    { name: 'Group', link: 'Website Pictures/20221102_122424.jpg'},
+    { name: 'Xena', price: '$2700', gender: 'Female', link: 'Website Pictures/20220602_205650.jpg'},
+    { name: 'Group', price: '$2700', gender: 'Female', link: 'Website Pictures/20221102_122424.jpg'},
+    { name: 'Xena', price: '$2700', gender: 'Female', link: 'Website Pictures/20220602_205650.jpg'},
+    { name: 'Opal', price: '$2700', gender: 'Female', link: 'Website Pictures/Nali Litter 2/Opal (3 Weeks).jpg'},
+    { name: 'Group', price: '$2700', gender: 'Female', link: 'Website Pictures/20221102_122424.jpg'},
 ];
 
 // Create the carousel items and html
@@ -22,6 +22,22 @@ items.forEach(item => {
     name.classList.add('carousel-name');
     name.textContent = `Name: ${item.name}`;
     carousel.appendChild(name);
+
+    const price = document.createElement('p');
+    price.classList.add('carousel-price');
+    price.textContent = `Price: ${item.price}`;
+    carousel.appendChild(price);
+
+    const gender = document.createElement('p');
+    gender.classList.add('carousel-gender');
+    gender.textContent = `Gender: ${item.gender}`;
+    carousel.appendChild(gender);
+
+    const order = document.createElement('a');
+    order.classList.add('carousel-order');
+    order.textContent = `Reserve`;
+    order.href = `mailto:cookiesandcreamdoodles@gmail.com?subject=Request: ${item.name}`
+    carousel.appendChild(order);
 })
 
 // Carousel navigation, created with help from copilot
@@ -38,6 +54,21 @@ const updateCarousel = () => {
     const names = document.querySelectorAll('.carousel-name');
     names.forEach((name, index) => {
         name.style.display = index === currentIndex? 'block' : 'none';
+    });
+    // Update the displayed price
+    const prices = document.querySelectorAll('.carousel-price');
+    prices.forEach((price, index) => {
+        price.style.display = index === currentIndex? 'block' : 'none';
+    });
+    // Update the displayed gender
+    const genders = document.querySelectorAll('.carousel-gender');
+    genders.forEach((gender, index) => {
+        gender.style.display = index === currentIndex? 'block' : 'none';
+    });
+    // Update the displayed order
+    const orders = document.querySelectorAll('.carousel-order');
+    orders.forEach((order, index) => {
+        order.style.display = index === currentIndex? 'block' : 'none';
     });
 };
 
